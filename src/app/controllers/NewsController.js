db62202422a670e3be3b48fa1e3b563e228ecbe7
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-04-17 09:41:45 +0700

    install code formatter prettier

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,10 +1,10 @@
 const NewsController = {
-    index: (req, res) => {
-        res.render('news');
-    },
-    show: (req, res) => {
-        res.send('news detail');
-            },
-};
+        index: (req, res) => {
+            res.render('news');
+          },
+          show: (req, res) => {
+              res.send('news detail');
+          },
+      };
 
 module.exports = NewsController;
